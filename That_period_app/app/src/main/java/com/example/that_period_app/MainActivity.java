28bb3e16374d66e94c5a16932e07ea5f0cc86bad
lmthys <nutmeggg14@gmail.com>
2024-11-11 20:10:22 -0600

    finally figured out permissions

--- That_period_app/app/src/main/java/com/example/that_period_app/MainActivity.java
+++ That_period_app/app/src/main/java/com/example/that_period_app/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.that_period_app;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+
+    private static final int REQUEST_CODE_PERMISSIONS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,54 @@ public class MainActivity extends WearableActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        String[] PERMISSIONS = {
+                Manifest.permission.BODY_SENSORS,
+                Manifest.permission.BODY_SENSORS_BACKGROUND
+        };
+
+        // Check if all permissions are granted
+        if (arePermissionGranted(PERMISSIONS))
+        {
+            Toast.makeText(this, "Permissions are already granted", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    private boolean arePermissionGranted(String[] permissions)
+    {
+        for (String permission: permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            boolean allGranted = true;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allGranted = false;
+                    break;
+                }
+            }
+
+            if (allGranted) {
+                Toast.makeText(this, "All permissions granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Some permissions denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
